c3d6133fac1428577e8b4ae66880cf86b17f37d9
payal101998 <104244767+payal101998@users.noreply.github.com>
2022-04-23 10:08:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('.btn btn-primary about');
+button.addEventListener('click',()=>{
+    window.open(about.html);
+
+})
+const centre = document.querySelector('.centre');
+centre.addEventListener('click',()=>{
+    window.open(construction.html);
+}
+)
+const artitech = document.querySelector('.artitech');
+artitech.addEventListener('click',()=>{
+    window.open(artitech.html);
+}
+)
+const drone = document.querySelector('.drone');
+drone.addEventListener('click',()=>{
+    window.open(drone.html);
+}
+)
